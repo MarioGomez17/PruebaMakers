ba69ba517d886d7c82d03b391bd6be6e55fcf97f
mariogomez441236 <mariogomez441236@gmail.com>
2025-02-21 09:58:56 -0500

    Second Commit

---
+++ PruebaTecnica.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaTecnica.API.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
